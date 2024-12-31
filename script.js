b1534ca04de56fabf82b747696861943e56395e6
Shubhkarman Jaura <jaura@ualberta.ca>
2024-12-31 16:01:54 -0600

    update

--- script.js
+++ script.js
@@ -73,17 +73,28 @@ $(document).ready(function(){
 });
 
 // Contact form handling with EmailJS
-document.querySelector("#contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission
+// document.querySelector("#contact-form").addEventListener("submit", function(event) {
+//     event.preventDefault(); // Prevent default form submission
 
-    console.log("Form submission started...");
+//     console.log("Form submission started...");
 
-    emailjs.sendForm("service_4j0u6qp", "template_ph9376b", this)
-        .then(function(response) {
-            console.log("SUCCESS!", response.status, response.text);
-            alert("Message sent successfully!");
-        }, function(error) {
-            console.error("FAILED...", error);
-            alert("Failed to send message. Please try again later.");
-        });
-});
+//     emailjs.sendForm("service_4j0u6qp", "template_ph9376b", this)
+//         .then(function(response) {
+//             console.log("SUCCESS!", response.status, response.text);
+//             alert("Message sent successfully!");
+//         }, function(error) {
+//             console.error("FAILED...", error);
+//             alert("Failed to send message. Please try again later.");
+//         });
+// });
+
+function sendmail(){
+    var params = {
+        from_name : document.getElementbyId("full_name").value;
+        reply_to: document.getElementbyId("email_id").value;
+        message: document.getElementbyId("email_message").value;
+    }
+    emailjs.sendForm("service_4j0u6qp", "template_ph9376b", params).then(function(res){
+        alert("Success!");
+    })
+}
